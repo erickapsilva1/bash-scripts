fbc03982c55b6da41a4865f86d954dd26a0f004f
Erick Silva <erick.ap.silva@gmail.com>
2021-11-14 12:26:49 -0300

    Form improvement

--- Intermediate/form.sh
+++ Intermediate/form.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-echo -n 'Name: '
+tput clear
+
+#tput setaf 0;echo color 0
+#tput setaf 1;echo color 1
+#tput setaf 2;echo color 2
+#tput setaf 3;echo color 3
+#tput setaf 4;echo color 4
+#tput setaf 5;echo color 5
+#tput setaf 6;echo color 6
+#tput setaf 7;echo color 7
+
+tput setaf 0
+tput setab 6
+
+echo "                                 Personal Data                                 "
+echo " $(tput setab 0)                                                                             $(tput setab 6) "
+echo " $(tput setab 0;tput setaf 7;tput bold)     Name:  $(tput setab 7)                                                                $(tput setab 0) $(tput setab 6) "
+echo " $(tput setab 0)    Email: $(tput setab 7)                                                                 $(tput setab 0) $(tput setab 6) "
+echo " $(tput setab 0) Password: $(tput setab 7)                                                                 $(tput setab 0) $(tput setab 6) "
+echo " $(tput setab 0)                                                                             $(tput setab 6) "
+echo " $(tput setab 0)                                                                             $(tput setab 6) "
+echo "                                                                               "
+
+tput cup 2 12
 read name
 
-echo -n 'E-mail: '
+tput cup 3 12
 read email
 
-echo -n 'Password: '
+tput cup 4 12
 read -s password
 
+echo
+echo
+echo
 echo
 echo "Collected data: ' $name / $email / $password."
